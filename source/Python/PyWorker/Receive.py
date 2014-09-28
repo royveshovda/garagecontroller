dbeb8414cfd26e6d2eea71b7dc12b9d245032805
Roy Veshovda <roy@veshovda.no>
2014-09-28 21:50:30 +0200

    Python: AMQP

--- source/Python/PyWorker/Receive.py
+++ source/Python/PyWorker/Receive.py
@@ -5,6 +5,7 @@ import signal
 from time import sleep
 
 from kombu import Connection, Queue
+from kombu.mixins import ConsumerMixin
 from kombu.pools import producers
 #import pifacedigitalio as pi_face
 
@@ -22,30 +23,35 @@ def start_receiving(filename):
 
     queue = Queue(queue_name)
 
-
-
-
-    with Connection(connection_string) as conn:
+    with Connection(connection_string, heartbeat=20) as conn:
         reconnect(conn)
-        with conn.Consumer(queue, callbacks=[process_message]) as consumer:
-            with producers[conn].acquire(block=True) as producer:
-                running = True
-                print("Running\n")
-                heartbeat = datetime.datetime.utcnow()
-                consumer.consume()
-                while running:
-                    reconnect(conn)
-                    temp_heartbeat = datetime.datetime.utcnow()
-                    if (temp_heartbeat - heartbeat).total_seconds() > 30:
-                        heartbeat = temp_heartbeat
-                        send_heartbeat(producer, device_id)
-                    try:
-                        conn.drain_events(timeout=5)
-                    except KeyboardInterrupt:
-                        running = False
-                    except socket.timeout:
-                        #conn.heartbeat_check()
-                        running = True
+        worker = Worker(conn, queue)
+        try:
+            print("Running")
+            worker.run()
+        except KeyboardInterrupt:
+            print("Exiting")
+
+
+        # with conn.Consumer(queue, callbacks=[process_message]) as consumer:
+        #     with producers[conn].acquire(block=True) as producer:
+        #         running = True
+        #         print("Running\n")
+        #         heartbeat = datetime.datetime.utcnow()
+        #         consumer.consume()
+        #         while running:
+        #             reconnect(conn)
+        #             temp_heartbeat = datetime.datetime.utcnow()
+        #             if (temp_heartbeat - heartbeat).total_seconds() > 30:
+        #                 heartbeat = temp_heartbeat
+        #                 send_heartbeat(producer, device_id)
+        #             try:
+        #                 conn.drain_events(timeout=9)
+        #             except KeyboardInterrupt:
+        #                 running = False
+        #             except socket.timeout:
+        #                 #conn.heartbeat_check()
+        #                 running = True
 
 
 def reconnect(local_connection):
@@ -95,6 +101,17 @@ def set_exit_handler(func):
 def on_exit(sig, func=None):
     print("exit handler triggered")
     sys.exit(1)
+
+
+class Worker(ConsumerMixin):
+    def __init__(self, connection, queue):
+        self.connection = connection
+        self.queue = queue
+
+    def get_consumers(self, Consumer, channel):
+        return [
+            Consumer(self.queue, callbacks=[process_message], accept=['json']),
+        ]
 
 
 if __name__ == '__main__':
